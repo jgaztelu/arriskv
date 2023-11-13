0e5f5397b086c78699c3f7803cbcf53552fe7abd
jgaztelu <jgazte@gmail.com>
2023-11-13 01:34:04 +0100

    Add decoded_op type input to ALU and branching. TODO: Use new input instead of old argx ports.

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -10,6 +10,7 @@ module alu #(
     // Input arguments
     input  logic        [wd_regs_p-1:0] i_arg1,
     input  logic        [wd_regs_p-1:0] i_arg2,
+    input  decoded_op_t                 i_op,
     // Instruction
     input  instr_t                      i_instr,
     input  instr_type_t                 i_instr_type,
@@ -17,61 +18,60 @@ module alu #(
     output logic        [wd_regs_p-1:0] o_rdest
 
 );
-  logic [wd_regs_p-1:0] arg1_se;
-  logic [wd_regs_p-1:0] arg2_se;
-  logic [          4:0] shamt;  // Shift amount
+    logic [wd_regs_p-1:0] arg1_se;
+    logic [wd_regs_p-1:0] arg2_se;
+    logic [          4:0] shamt;  // Shift amount
 
-  assign shamt = i_arg2[4:0];
+    assign shamt = i_arg2[4:0];
 
-  always_ff @(posedge clk) begin : arith_proc
-    if (!rst_n) begin
-      o_result <= '0;
-    end else begin
-      // Default assignments
-      o_result <= '0;
-      case (i_instr)
-        // Register-immediate
-        ADDI: o_result <= i_arg1 + arg2_se;
-        SLTI: o_result <= (i_arg1 < arg2_se) ? 1 : 0;  // TODO: Convert to signed comparison!
-        SLTIU: o_result <= (i_arg1 < arg2_se) ? 1 : 0;
-        ANDI: o_result <= i_arg1 & arg2_se;
-        ORI: o_result <= i_arg1 | arg2_se;
-        XORI: o_result <= i_arg1 ^ arg2_se;
-        SLLI: o_result <= i_arg1 << shamt;
-        SRLI: o_result <= i_arg1 >> shamt;
-        SRAI: o_result <= i_arg1 >>> shamt;  // TODO: Check with signed inputs
-        LUI:
-        o_result <= {i_arg2[19:0], 12'b0};  // TODO: Think about separate port for immediates?  
+    always_ff @(posedge clk) begin : arith_proc
+        if (!rst_n) begin
+            o_result <= '0;
+        end else begin
+            // Default assignments
+            o_result <= '0;
+            case (i_instr)
+                // Register-immediate
+                ADDI:  o_result <= i_arg1 + arg2_se;
+                SLTI:  o_result <= (i_arg1 < arg2_se) ? 1 : 0;  // TODO: Convert to signed comparison!
+                SLTIU: o_result <= (i_arg1 < arg2_se) ? 1 : 0;
+                ANDI:  o_result <= i_arg1 & arg2_se;
+                ORI:   o_result <= i_arg1 | arg2_se;
+                XORI:  o_result <= i_arg1 ^ arg2_se;
+                SLLI:  o_result <= i_arg1 << shamt;
+                SRLI:  o_result <= i_arg1 >> shamt;
+                SRAI:  o_result <= i_arg1 >>> shamt;  // TODO: Check with signed inputs
+                LUI:   o_result <= {i_arg2[19:0], 12'b0};  // TODO: Think about separate port for immediates?  
 
-        // Register-register
-        ADD:   o_result <= i_arg1 + i_arg2;
-        SLT:   o_result <= (i_arg1 < i_arg2) ? 1 : 0;  // TODO: Convert to signed comparison!
-        SLTU:  o_result <= (i_arg1 < i_arg2) ? 1 : 0;
-        AND:   o_result <= i_arg1 & i_arg2;
-        OR:    o_result <= i_arg1 | i_arg2;
-        XOR:   o_result <= i_arg1 ^ i_arg2;
-        SLLI:  o_result <= i_arg1 << shamt;  // TODO: Ensure shamt comes from reg and not immediate?
-        SRLI:  o_result <= i_arg1 >> shamt;
-        SRA:   o_result <= i_arg1 >>> shamt;  // TODO: Check with signed inputs
-        SUB:   o_result <= i_arg1 - i_arg2;
-        AUIPC: o_result <= i_pc + {i_arg2[19:0], 12'b0};
+                // Register-register
+                ADD: o_result <= i_arg1 + i_arg2;
+                SLT: o_result <= (i_arg1 < i_arg2) ? 1 : 0;  // TODO: Convert to signed comparison!
+                SLTU: o_result <= (i_arg1 < i_arg2) ? 1 : 0;
+                AND: o_result <= i_arg1 & i_arg2;
+                OR: o_result <= i_arg1 | i_arg2;
+                XOR: o_result <= i_arg1 ^ i_arg2;
+                SLLI: o_result <= i_arg1 << shamt;  // TODO: Ensure shamt comes from reg and not immediate?
+                SRLI: o_result <= i_arg1 >> shamt;
+                SRA: o_result <= i_arg1 >>> shamt;  // TODO: Check with signed inputs
+                SUB: o_result <= i_arg1 - i_arg2;
+                AUIPC: o_result <= i_pc + {i_arg2[19:0], 12'b0};
 
-        // New address calculation / Jumps
-        JAL:  o_result <= i_pc + arg2_se[20:0];  // TODO: Implement actual jump behavior
-        JALR: o_result <= i_arg1 + arg2_se[11:0];  // TODO: Implement actual jump behavior
+                // New address calculation / Jumps
+                JAL:  o_result <= i_pc + arg2_se[20:0];  // TODO: Implement actual jump behavior
+                JALR: o_result <= i_arg1 + arg2_se[11:0];  // TODO: Implement actual jump behavior
 
-        default: o_result <= '0;
-      endcase
+                default: o_result <= '0;
+            endcase
+        end
     end
-  end
 
 
-  sign_extend #(
-      .wd_regs_p(wd_regs_p)
-  ) sign_extend_i (
-      .i_immediate (i_arg2),
-      .i_instr_type(i_instr_type),
-      .o_sign_ext  (arg2_se)
-  );
+    sign_extend #(
+        .wd_regs_p(wd_regs_p)
+    ) sign_extend_i (
+        .i_immediate (i_arg2),
+        .i_instr_type(i_instr_type),
+        .o_sign_ext  (arg2_se)
+    );
 
 endmodule
--- rtl/branching.sv
+++ rtl/branching.sv
@@ -3,47 +3,50 @@ import arriskv_pkg::*;
 module branching #(
     parameter int wd_regs_p = 32
 ) (
-    input  logic                   clk,
-    input  logic                   rst_n,
+    input  logic                        clk,
+    input  logic                        rst_n,
     // Program counter
-    input  logic   [wd_regs_p-1:0] i_pc,
-    output logic   [wd_regs_p-1:0] o_pc,
-    output logic                   o_br_taken,  // Branch taken, set PC to new value
+    input  logic        [wd_regs_p-1:0] i_pc,
+    output logic        [wd_regs_p-1:0] o_pc,
+    output logic                        o_br_taken,  // Branch taken, set PC to new value
     // Input arguments
-    input  logic   [wd_regs_p-1:0] i_arg1,
-    input  logic   [wd_regs_p-1:0] i_arg2,
-    input  logic                   i_jump,      // Jump type operation 
+    input  logic        [wd_regs_p-1:0] i_arg1,
+    input  logic        [wd_regs_p-1:0] i_arg2,
+    input  decoded_op_t                 i_op,
+    input  logic                        i_jump,      // Jump type operation 
     // Instruction
-    input  instr_t                 i_instr
+    input  instr_t                      i_instr
     //input  instr_type_t     i_instr_type
 );
 
-  always_ff @(posedge clk) begin : branching_proc
-    if (!rst_n) begin
-      o_pc <= '0;
-      o_br_taken <= 0;
-    end else begin
-      if (i_jump) begin
-        case (i_instr)
-          JAL: o_pc <= i_pc + i_arg2[19:0] + 4;
-          JALR: o_pc <= i_arg1 + i_arg2[11:0] + 4;  // TODO: Need to add sign extension
-          BEQ:
-          o_pc <= (i_arg1 == i_arg2) ? i_pc + i_arg2[12:0] : i_pc; // TODO: Add immediate port, substitue i_arg2  
-          BNE:
-          o_pc <= (i_arg1 != i_arg2) ? i_pc + i_arg2[12:0] : i_pc; // TODO: Add immediate port, substitue i_arg2    
-          BLT: o_pc <= (i_arg1 < i_arg2) ? i_pc + i_arg2[12:0] : i_pc;  // TODO: Make signed 
-          BGE: o_pc <= (i_arg1 >= i_arg2) ? i_pc + i_arg2[12:0] : i_pc;  // TODO: Make signed  
-          BLTU:
-          o_pc <= (i_arg1 < i_arg2) ? i_pc + i_arg2[12:0] : i_pc; // TODO: Add immediate port, substitue i_arg2  
-          BGEU:
-          o_pc <= (i_arg1 >= i_arg2) ? i_pc + i_arg2[12:0] : i_pc; // TODO: Add immediate port, substitue i_arg2  
-        endcase
+    always_ff @(posedge clk) begin : branching_proc
+        if (!rst_n) begin
+            o_pc <= '0;
+            o_br_taken <= 0;
+        end else begin
+            if (i_jump) begin
+                case (i_instr)
+                    JAL: o_pc <= i_pc + i_arg2[19:0] + 4;
+                    JALR: o_pc <= i_arg1 + i_arg2[11:0] + 4;  // TODO: Need to add sign extension
+                    BEQ:
+                    o_pc <= (i_arg1 == i_arg2) ? i_pc + i_arg2[12:0] : i_pc; // TODO: Add immediate port, substitue i_arg2  
+                    BNE:
+                    o_pc <= (i_arg1 != i_arg2) ? i_pc + i_arg2[12:0] : i_pc; // TODO: Add immediate port, substitue i_arg2    
+                    BLT:
+                    o_pc <= (i_arg1 < i_arg2) ? i_pc + i_arg2[12:0] : i_pc;  // TODO: Make signed 
+                    BGE:
+                    o_pc <= (i_arg1 >= i_arg2) ? i_pc + i_arg2[12:0] : i_pc;  // TODO: Make signed  
+                    BLTU:
+                    o_pc <= (i_arg1 < i_arg2) ? i_pc + i_arg2[12:0] : i_pc; // TODO: Add immediate port, substitue i_arg2  
+                    BGEU:
+                    o_pc <= (i_arg1 >= i_arg2) ? i_pc + i_arg2[12:0] : i_pc; // TODO: Add immediate port, substitue i_arg2  
+                endcase
 
-        o_br_taken <= 1;
-      end else begin
-        o_pc <= i_pc;
-        o_br_taken <= 0;
-      end
+                o_br_taken <= 1;
+            end else begin
+                o_pc <= i_pc;
+                o_br_taken <= 0;
+            end
+        end
     end
-  end
 endmodule
