52711ef02eb88d5c0fcff13c03cea25366763158
jgaztelu <jgazte@gmail.com>
2023-11-13 01:34:27 +0100

    Add reg_file to top

--- rtl/arriskv_top.sv
+++ rtl/arriskv_top.sv
@@ -2,41 +2,68 @@ import arriskv_pkg::*;
 
 module arriskv_top #(
     parameter int wd_regs_p = 32,
-    parameter int n_regs_p = 32
+    parameter int n_regs_p = 32,
+    localparam int n_reg_rd_ports = 2,
+    localparam int n_reg_wr_ports = 2,
+    localparam int wd_addr_p = $clog2(n_regs_p)
 ) (
     input logic clk,
     input logic rst_n
 );
 
-decoded_op_t decoded_op;
+    decoded_op_t                                     decoded_op;
 
-instr_decode #(
-    .wd_instr_p    (32),
-    .n_regs_p      (32),
-    .wd_regs_p     (32),
-    .n_rd_ports    (2),
-    .wd_addr_p     (n_regs_p)
-) instr_decode_i (
-    .clk(clk),
-    .rst_n(rst_n),
-    .i_instr(i_instr),
-    .o_reg_rd_addr(),
-    .i_reg_rd_data(),
-    .o_instr(decoded_op)
-);
+    // Register file read/write signals
+    logic        [n_reg_rd_ports-1:0][wd_addr_p-1:0] reg_rd_addr;
+    logic        [n_reg_rd_ports-1:0][wd_regs_p-1:0] reg_rd_data;
+    logic        [n_reg_wr_ports-1:0]                reg_wr_en;
+    logic        [n_reg_wr_ports-1:0][wd_addr_p-1:0] reg_wr_addr;
+    logic        [n_reg_wr_ports-1:0][wd_regs_p-1:0] reg_wr_data;
 
-execute #(
-    .wd_regs_p    (32)
-) execute_i (
-    .clk          (clk),
-    .rst_n        (rst_n),
-    // Inputs from register file
-    .i_reg1       (i_reg1),
-    .i_reg2       (i_reg2),
-    // Decoded instruction
-    .i_pc         (i_pc),
-    // Decoded operation
-    .i_dec_op     (decoded_op)
-);
+    reg_file #(
+        .n_regs_p  (n_regs_p),
+        .wd_regs_p (wd_regs_p),
+        .n_rd_ports(n_reg_rd_ports),
+        .n_wr_ports(n_reg_wr_ports)
+    ) reg_file_i (
+        .clk(clk),
+        .rst_n(rst_n),
+        // Read ports
+        .i_reg_rd_addr(reg_rd_addr),
+        .o_reg_rd_data(reg_rd_data),
+        // Write ports
+        .i_reg_wr_en(reg_wr_en),
+        .i_reg_wr_addr(reg_wr_addr),
+        .i_reg_wr_data(reg_wr_data)
+    );
+
+    instr_decode #(
+        .wd_instr_p(32),
+        .n_regs_p  (n_regs_p),
+        .wd_regs_p (wd_regs_p),
+        .n_rd_ports(2)
+    ) instr_decode_i (
+        .clk(clk),
+        .rst_n(rst_n),
+        .i_instr(i_instr),
+        .o_reg_rd_addr(reg_rd_addr),
+        .i_reg_rd_data(),
+        .o_instr(decoded_op)
+    );
+
+    execute #(
+        .n_regs_p  (n_regs_p),
+        .wd_regs_p (wd_regs_p),
+        .n_rd_ports(n_reg_rd_ports)
+    ) execute_i (
+        .clk          (clk),
+        .rst_n        (rst_n),
+        // Inputs from register file
+        .i_reg_rd_data(reg_rd_data),
+        // Decoded instruction
+        .i_pc         (i_pc),
+        // Decoded operation
+        .i_dec_op     (decoded_op)
+    );
 
 endmodule
